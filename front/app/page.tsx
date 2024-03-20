10e7cba3771f3f050b685057bd06ff860eb7cd51
samuel_ndjouli <samuelndj.997@gmail.com>
2024-03-20 16:58:26 +0100

    add quizz

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,72 +1,81 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
+const FormSchema = z.object({
+  type: z.enum(["all", "mentions", "none"], {
+    required_error: "You need to select a notification type.",
+  }),
+})
 
 export default function Home() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  })
+ 
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <section className="container mx-auto px-8 pt-5 pb-8 lg:pt-[90px] lg:pb-[110px] lg:px-[110px]">
         <div className="flex flex-col justify-center items-center space-y-8">
           <h1 className="bg-clip-text text-transparent bg-gradient-to-br from-white to-[#52525B] text-3xl lg:text-6xl font-bold text-center">Bienvenue sur notre quizz</h1>
           <div>
-          <form>
-              <div className="flex items-center">
-                <input
-                  id="default-radio-1"
-                  type="radio"
-                  value=""
-                  name="default-radio"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  htmlFor="default-radio-1"
-                  className="ms-2 text-sm font-medium text-white"
-                  >Géométrie</label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  checked
-                  id="default-radio-2"
-                  type="radio"
-                  value=""
-                  name="default-radio"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  htmlFor="default-radio-2"
-                  className="ms-2 text-sm font-medium text-white"
-                  >Mathématiques</label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  checked
-                  id="default-radio-2"
-                  type="radio"
-                  value=""
-                  name="default-radio"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  htmlFor="default-radio-2"
-                  className="ms-2 text-sm font-medium text-white"
-                  >Histoire</label>
-              </div>
-              <div className="flex items-center">
-                <input
-                  checked
-                  id="default-radio-2"
-                  type="radio"
-                  value=""
-                  name="default-radio"
-                  className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                />
-                <label
-                  htmlFor="default-radio-2"
-                  className="ms-2 text-sm font-medium text-white"
-                  >Culture Générale</label>
-              </div>
+          <Form 
+            {...form}
+            >
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+              <FormField
+                control={form.control}
+                name="type"
+                render={({ field }) => (
+                  <FormItem className="space-y-3">
+                    <FormLabel>Choisissez votre sujet</FormLabel>
+                    <FormControl>
+                      <RadioGroup
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                        className="flex flex-col space-y-1"
+                      >
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="mentions" />
+                          </FormControl>
+                          <FormLabel className="font-normal">
+                            Direct messages and mentions
+                          </FormLabel>
+                        </FormItem>
+                        <FormItem className="flex items-center space-x-3 space-y-0">
+                          <FormControl>
+                            <RadioGroupItem value="none" />
+                          </FormControl>
+                          <FormLabel className="font-normal">Nothing</FormLabel>
+                        </FormItem>
+                      </RadioGroup>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className="mt-8 bg-gradient-to-r from-[#eeaeca] to-[#94bbe9]" type="submit">Commencer le quizz</Button>
             </form>
+          </Form>
           </div>
-          <Button className="mt-8 bg-gradient-to-r from-[#eeaeca] to-[#94bbe9]">Commencer le quizz</Button>
         </div>
       </section>
     </main>
